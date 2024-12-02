e25204847d4040d3f5dfe05b4219653d4ca3d254
edy alves <30.edyalves@gmail.com>
2024-12-02 14:24:20 -0300

    Initial Commit

---
+++ Uteis/UteisApp.swift
@@ -0,0 +1,17 @@
+//
+//  UteisApp.swift
+//  Uteis
+//
+//  Created by ednardo alves on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UteisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
